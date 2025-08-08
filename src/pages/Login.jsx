e2943ad178509f90295d7980a4fca5c6489aad81
priyan03 <priyanpaulsamuel333@gmail.com>
2025-08-08 14:47:20 +0530

    Updated Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { TextField, InputAdornment } from '@mui/material';
+import { TextField , Checkbox, FormControlLabel} from '@mui/material';
 import Button from '@mui/material/Button';
 import { UserRoundSearch, Lock, CircleUserRound} from 'lucide-react';
 import "../styles/LoginStyle.css";
@@ -7,9 +7,11 @@ const Login = () => {
     const Submits = () =>{
         console.log("Logged");
     }
+    const Register = () =>{
+        console.log("Register")
+    }
     return(
         <div className="input-wrapper">
-            
             <div className="border">
                 <div className="box">
 
@@ -28,8 +30,11 @@ const Login = () => {
                     <div className="pass">
                         <Lock size={16} className="icon3" />
                         <TextField label= "Password" variant="outlined" type="password" size="small" fullWidth/>
+                        <FormControlLabel control={<Checkbox size="smaller" />}/><span className="span" style={{fontSize:"0.6rem"}}>Remember me</span><a className="a1" href="#" style={{fontSize:"0.6rem", color:"#1976d2"}}>Forgot password?</a>
+
                     </div>
                         <Button className="button-field" variant="contained" onClick={Submits} size="small">Sign In</Button>
+                        <p style={{fontSize : "0.7rem"}}>Don't have an account? <a href="#" className="a"><span onClick={Register} style={{fontWeight: "Bold", color:"#1976d2"}}>Register</span></a></p>
                 </div>
             </div>     
         </div>
