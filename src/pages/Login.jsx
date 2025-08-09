215e964739cfa16f8d7f514021318e8158293ff5
priyan03 <priyanpaulsamuel333@gmail.com>
2025-08-09 12:03:58 +0530

    Updated Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,42 +1,49 @@
 import * as React from "react";
-import { TextField , Checkbox, FormControlLabel} from '@mui/material';
+import logo from '../Logo Priyan webX.png';
+import bgGif from '../bgGif/bg.gif';
+import { TextField, Checkbox, FormControlLabel, colors } from '@mui/material';
 import Button from '@mui/material/Button';
-import { UserRoundSearch, Lock, CircleUserRound} from 'lucide-react';
+import { UserRoundSearch, Lock, CircleUserRound } from 'lucide-react';
 import "../styles/LoginStyle.css";
 const Login = () => {
-    const Submits = () =>{
+    const Submits = () => {
         console.log("Logged");
     }
-    const Register = () =>{
+    const Register = () => {
         console.log("Register")
     }
-    return(
-        <div className="input-wrapper">
-            <div className="border">
-                <div className="box">
-
-                    <UserRoundSearch size={62} className="icon"/>
+    return (
+        <div
+            className="backgroundimage"
+            style={{ backgroundImage: `url(${bgGif})` }}>
                 
-                    <h1 className="bt">
-                        <span className="b">P</span>riyan <span className="b">W</span>eb
-                    </h1>
-                    <div className="si">Sign in to your account</div>
+            <div className="input-wrapper">
+                <div className="img"><img className="img" src={logo} alt="Logo" title="Priyan webX Logo" /></div>
+                <div className="border">
+                    <div className="box">
+                        <UserRoundSearch size={62} className="icon" />
+                        <h1 className="bt">
+                            <span className="b">P</span>riyan <span className="b">W</span>eb<span style={{ fontSize: "3rem", color: "GrayText" }}>X</span>
+                        </h1>
+                        <span className="si">Sign in to your account</span>
 
-                    <div className="email">  
-                        <CircleUserRound size={17} className="icon2"/>
-                        <TextField label= "Enter e-mail address" variant="outlined" type="email" size="small" fullWidth/>
-                    </div> 
-                
-                    <div className="pass">
-                        <Lock size={16} className="icon3" />
-                        <TextField label= "Password" variant="outlined" type="password" size="small" fullWidth/>
-                        <FormControlLabel control={<Checkbox size="smaller" />}/><span className="span" style={{fontSize:"0.6rem"}}>Remember me</span><a className="a1" href="#" style={{fontSize:"0.6rem", color:"#1976d2"}}>Forgot password?</a>
+                        <div className="email">
+                            <CircleUserRound size={17} className="icon2" />
+                            <TextField label="Enter e-mail address" variant="outlined" type="email" size="small" fullWidth />
+                        </div>
 
-                    </div>
+                        <div className="pass">
+                            <Lock size={16} className="icon3" />
+                            <TextField label="Password" variant="outlined" type="password" size="small" fullWidth />
+                            <FormControlLabel control={<Checkbox size="smaller" />} /><span className="span" style={{ fontSize: "0.6rem" }}>Remember me</span>
+                            <span><a className="a1" href="#" style={{ fontSize: "0.6rem", color: "#1976d2" }}>Forgot password?</a></span>
+
+                        </div>
                         <Button className="button-field" variant="contained" onClick={Submits} size="small">Sign In</Button>
-                        <p style={{fontSize : "0.7rem"}}>Don't have an account? <a href="#" className="a"><span onClick={Register} style={{fontWeight: "Bold", color:"#1976d2"}}>Register</span></a></p>
+                        <p style={{ fontSize: "0.7rem" }}>Don't have an account? <a href="#" className="a"><span onClick={Register} style={{ fontWeight: "Bold", color: "#1976d2" }}>Register</span></a></p>
+                    </div>
                 </div>
-            </div>     
+            </div>
         </div>
     );
 };
