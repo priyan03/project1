f5e9824e5cb4a655ac7152839577ea00f5ff7c2f
priyan03 <priyanpaulsamuel333@gmail.com>
2025-08-11 20:26:12 +0530

    Updated Register.jsx

---
+++ src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import React,{ useState } from 'react'
+import '../styles/RegisterStyle'
+
+const Register = () => {
+  return (
+    <div>
+        <h1>Register Page</h1>
+      <p>This is where the registration form will be.</p>
+    </div>
+  )
+}
+
+export default Register;
