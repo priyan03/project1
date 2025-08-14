b8fc2a3b1ec2b5311ed0d65925bcba076a63da75
priyan03 <priyanpaulsamuel333@gmail.com>
2025-08-14 12:55:03 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-import { Routes, Route, Navigate } from "react-router-dom"
-
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-
-import "./styles/LoginStyle.css"
-import "./styles/RegisterStyle.css"
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home  from "./Components/Home";
 
 function App() {
-
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/login" replace />}/>
-      <Route path="/login" element={<Login/>}/> 
-      <Route path="/register"element={<Register/>}/>
+      <Route path="/" element={<Navigate to="/home" replace />} />
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/login" element={<Login />} /> 
+      <Route path="/register" element={<Register />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
