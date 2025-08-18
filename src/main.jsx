6357f257237d2fed3b4f5f3b43136406c6dd8938
priyan03 <priyanpaulsamuel333@gmail.com>
2025-08-18 11:28:14 +0530

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 
-const rootElement = document.getElementById('root');
-
-createRoot(rootElement).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
